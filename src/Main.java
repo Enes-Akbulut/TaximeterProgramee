8cdcbb06f3e1484497e49cd816345f73eb723b22
Enes Akbulut <107497616+Enes-Akbulut@users.noreply.github.com>
2022-06-23 13:36:38 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       //taksimetre programı:
+
+        double gidilenKM,tutar, minOdenecek,baslangıcUcretı, odnecekNetTutar;
+
+        Scanner girdi = new Scanner(System.in);
+
+        System.out.print("gidilen km = " );
+        gidilenKM = girdi.nextDouble();
+
+        tutar = gidilenKM *  2.20;
+        minOdenecek =20;
+        baslangıcUcretı = 10;
+        odnecekNetTutar = tutar+baslangıcUcretı;
+
+        boolean sonuc= (tutar>20);
+        double son = sonuc ? odnecekNetTutar:minOdenecek;
+        System.out.println("km basina odenecek tutar = 2.20");
+        System.out.println("gidilen kilometre= "+gidilenKM);
+        System.out.println("baslangic ucreti= "+baslangıcUcretı);
+        System.out.println("odenecek tutar=  "+ son);
     }
 }
